6c26601400f26206290363ca5850b57957053e6e
Laptop <Laptop@DESKTOP-AAIMPLJ>
2024-02-03 17:30:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class ejercicio4
+{
+    static void Main()
+    {
+        Console.Write("Introduce la base del cuadrado: ");
+        double baseCuadrado = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Introduce la altura del cuadrado: ");
+        double alturaCuadrado = Convert.ToDouble(Console.ReadLine());
+
+        double areaCuadrado = baseCuadrado * alturaCuadrado;
+
+        Console.WriteLine("El área del cuadrado es: " + areaCuadrado);
+    }
+}
